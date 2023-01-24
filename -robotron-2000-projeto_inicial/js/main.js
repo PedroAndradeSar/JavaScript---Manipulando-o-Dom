194d1c1bc776422c5cbaed13b4cbd39e8d98dd5c
Pedro Henrique Andrade Sartorato <phandradesar@gmail.com>
2023-01-24 09:09:26 -0300

    Aula de eventos com JavaScrit - inserção do main.js

---
+++ -robotron-2000-projeto_inicial/js/main.js
@@ -0,0 +1,9 @@
+const robotron = document.querySelector("#idrobotron")
+
+robotron.addEventListener("click", dizOi);
+
+function dizOi() {  
+    alert("oi")
+    console.log("teste com console.log")
+}
+
